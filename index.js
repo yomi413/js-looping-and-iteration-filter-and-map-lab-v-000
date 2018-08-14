7ef00e2c35794b743086066701e959d18cc282ca
Yomaira Escano <yjescano@gmail.com>
2018-08-14 17:24:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function driversWithRevenueOver(drivers, revenue) {
 
 function driverNamesWithRevenueOver(drivers, revenue) {
   return drivers.filter(function (driver) {
-    return driver;
+    return driver.name;
   });
 }
