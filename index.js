d1761b2dc90385f96f9a771cf1da60bc1916f309
Yomaira Escano <yjescano@gmail.com>
2018-08-14 16:48:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here:
 function driversWithRevenueOver(driverOjects, revenue) {
-  return driverObjects.map(function (driver) {
+  return driverObjects.filter(function (driver) {
     return driver.revenue > revenue;
   });
 }
